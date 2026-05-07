cffdbfdbe7451a937c2264aeeff4999a04557435
rzryhn <rizarayhanw@gmail.com>
2026-05-07 14:57:15 +0700

    create new arrivals

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import Hero from "../components/Layout/Hero";
 import GenderCollectionSection from "../components/Products/GenderCollectionSection";
+import NewArrivals from "../components/Products/NewArrivals";
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       {/* Gender Collection */}
       <GenderCollectionSection />
       {/* New Arrivals */}
+      <NewArrivals />
       {/* Best Seller */}
       {/* Also like */}
       {/* Top wear for women */}
