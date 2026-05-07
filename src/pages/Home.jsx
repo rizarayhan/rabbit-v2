d315f55aa31dbfa2492f929c065578555910cbcc
rzryhn <rizarayhanw@gmail.com>
2026-05-07 15:42:23 +0700

    create prodct details

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import Hero from "../components/Layout/Hero";
 import GenderCollectionSection from "../components/Products/GenderCollectionSection";
 import NewArrivals from "../components/Products/NewArrivals";
+import ProductDetails from "../components/Products/ProductDetails";
 
 const Home = () => {
   return (
@@ -12,6 +13,8 @@ const Home = () => {
       {/* New Arrivals */}
       <NewArrivals />
       {/* Best Seller */}
+      <h2 className="text-3xl text-center font-bold mb-4">Best Seller</h2>
+      <ProductDetails />
       {/* Also like */}
       {/* Top wear for women */}
       {/* Features section */}
